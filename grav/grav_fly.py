b9a0483b6a0c3ef02e1abd942fe91ec8b8f3f1e4
Olegpgud <olegpgud@rambler.ru>
2019-12-23 16:32:46 +0300

    fixed some bugs

--- grav/grav_fly.py
+++ grav/grav_fly.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 # -*- coding: UTF-8 -*-
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import numpy as np
 from astropy.io import fits
 from astropy.table import Table
 from astropy.wcs import WCS
 from astropy import units as u
+from astropy import constants as const
 from astropy.coordinates import SkyCoord
 from astropy.nddata import Cutout2D
 import astropy.io.fits.hdu.hdulist
@@ -15,27 +16,29 @@ from scipy.integrate import solve_ivp, RK23
 from scipy.optimize import curve_fit
 import os
 
+pi=np.pi
+G=const.G.value
+M0=const.M_sun.value
+
+n = 3.23984320 #sersic n
+Re = 5.44587833 * 3.08e19 #effective radius
+I0_sersic = 4967.29722 #I0
+
 def main(V0=None,alf=None,flag=None):
+    """
+    Input:
+        V0 - stars velocity
+        alf - angular of trajectory
+    Output:
+        date of trajectory and velocity
+        plot image 
+    """
     if V0 is None:
         V0 = float(input("input V0(m/sec): "))
     if alf is None:
         alf = float(input("input alf(degrees): "))
     if flag is None:
         flag = float(input("1 - plot image, 0 - not: "))
-
-
-    '''
-        Re:    5.44587833 (init= 5)
-        n:     3.23984320 (init= 4)
-        I0:    4967.29722 (init= 1000)
-        bkg:   1.49366509 (init= 10)
-    '''
-
-    pi = 3.14
-    G = 6.67*10**(-11)
-    M0 = 2*10**30
-
-    print('G = '+repr(G))
 
     path=os.path.dirname(os.path.abspath(__file__) )
     image_hst = fits.open(os.path.join(path,'hst_14219_34_wfc3_ir_f110w_drz.fits'),memmap=True)
@@ -45,54 +48,40 @@ def main(V0=None,alf=None,flag=None):
     phot = hst_header['PHOTFLAM']
     phot2 = hst_header['PHOTPLAM']
 
-    zeropoint = -2.5*np.log10(phot)-21.10-5*np.log10(phot2)+18.692
-    print('zeropoint = '+repr(zeropoint))
-    print('arc_pix = '+repr(arc_pix))
-    m = -2.5*np.log10(4967.29722)+zeropoint
+    zeropoint = -2.5*np.log10(phot)-21.10-5*np.log10(phot2)+18.692 #hubble zero point
+    m = -2.5*np.log10(I0_sersic)+zeropoint
     m_c = m+2.5*np.log10(arc_pix**2)
-    print('m = '+repr(m))
-    print('m_c ='+repr(m_c))
     m_abs = m - 5*np.log10((53.6*10**6)/10)-0.1
-    print('m_abs = '+repr(m_abs))
-    m_abs_m = m_abs+2.5*np.log10(arc_pix**2*0.260**2*9.5*10**38)
-    print('m_abs/m2 = '+repr(m_abs_m))
-    I0_c = 10**((m_abs_m-3.82)/-2.5)*(3.08*10**16)**2
-    print('I0_c = '+repr(I0_c))
+    m_abs_m = m_abs+2.5*np.log10(arc_pix**2*0.260**2*9.5e38)
+    I0_c = 10**((m_abs_m-3.82)/-2.5)*(3.08e16)**2
     I0 = 3.8*10**26*10**((m_abs_m-3.82)/-2.5)
-    print('I0 = '+repr(I0))
+    Re_pc = Re/3.08e16 
+    
 
-    Re = 5.44587833 * 3.08*10**19
-    Re_pc = 5445.87833
-    n = 3.23984320
-    print('n = '+repr(n))
-
-
-    b = 2*n-1./3+4./(405*n)+46./(25525*n**2)+131./(1148175*n**3)-2194697./(30690717750*n**4)
-    p = 1.0 - 0.6097/n+0.05563/n**2
+    b = 2*n-1./3+4./(405*n)+46./(25525*n**2)+131./(1148175*n**3)-2194697./(30690717750*n**4) #polinome
+    p = 1.0 - 0.6097/n+0.05563/n**2 #polinome
     M_L = 0.36
-    print('M/L = '+repr(M_L))
     q0_mpc = M_L*I0_c*b**(n*(1-p))*gamma(2*n)/(2*Re_pc*gamma(n*(3-p)))
-    q0 = q0_mpc*M0/(3.08*10**16)**3
-    print('Re = '+repr(Re))
-    print('b = '+repr(b)+' p = '+repr(p)+' q_mpc = '+repr(q0_mpc)+' q0 = '+repr(q0))
-
+    q0 = q0_mpc*M0/(3.08e16)**3
 
     def q(r):
+        """
+        rho function
+        """
         return q0*(r/Re)**(-p)*np.exp(-b*(r/Re)**(1/n))
-
 
     def L1(r):
         return q0*Re**2*n*b**(n*(p-2))*gammaincc(n*(2-p),b*(r/Re)**(1/n))*gamma(n*(2-p))
 
-
     f0 = -4*pi*G*L1(0)
-    print('f0 = '+repr(f0))
-
 
     def L2(r):
         return q0*Re**3*n*b**(n*(p-3))*(gamma(n*(3-p))*gammaincc(n*(3-p),0)-gamma(n*(3-p))*gammaincc(n*(3-p),b*(r/Re)**(1/n)))
 
     def f(r):
+        """
+        potencial fuction
+        """
         return -4*pi*G*(1/r*L2(r)+L1(r))
 
     def fun(t,r):
@@ -110,24 +99,28 @@ def main(V0=None,alf=None,flag=None):
 
     x0=np.array([1])
     sol = solve_ivp(fun, [t0, tf], x0, max_step=max_step)
-    print(sol)
-    vel = fun(sol.t,sol.y[0])
+    if (sol.success == False):
+        return
+    else:
+        vel = fun(sol.t,sol.y[0])
+        return vel 
     if (flag==1):
+        import matplotlib.pyplot as plt
+        print(sol)
         fig, axes = plt.subplots(figsize=(22,10), nrows=1, ncols=2)
         ax1 = axes[0]
         ax2 = axes[1]
-        ax1.plot(sol.t/(3.154*10**13), (sol.y[0]/(3.08*10**19))*(abs(np.cos(alf*0.0175))))
+        ax1.plot(sol.t/(3.154e13), (sol.y[0]/(3.08e19))*(abs(np.cos(alf*0.0175))))
         ax1.set_xlabel('time, millions yaers')
         ax1.set_ylabel('distance, kpc')
         ax1.set_title('R(t)')
-        #ax2.plot(sol.y[0]/(3.08*10**19), (vel/1000)*(abs(np.sin(alf*0.0175))))
-        ax2.plot(sol.t/(3.154*10**13), (vel/1000)*(abs(np.sin(alf*0.0175))))
+        ax2.plot(sol.t/(3.154e13), (vel/1000)*(abs(np.sin(alf*0.0175))))
         ax2.set_xlabel('time, millions yaers')
         ax2.set_ylabel('Velocity, km/sec')
         ax2.set_title('V(t)')
         plt.show()
 
-    return vel
+
 
 
 
